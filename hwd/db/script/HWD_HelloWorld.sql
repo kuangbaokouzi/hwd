f5e97983038369c60c61381c3109a67f2ab35d89
jasonwu <jasonwu@192.168.2.108>
2018-10-31 17:39:49 +0800

    修改table名称

--- hwd/db/script/HWD_HelloWorld.sql
+++ hwd/db/script/HWD_HelloWorld.sql
@@ -1,15 +1,15 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2018/10/31 ÐÇÆÚÈý 15:04:40                      */
+/* Created on:     2018/10/31 ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ 15:04:40                      */
 /*==============================================================*/
 
 
-drop table if exists HelloWorld;
+drop table if exists HWD_HelloWorld;
 
 /*==============================================================*/
 /* Table: HelloWorld                                            */
 /*==============================================================*/
-create table HelloWorld
+create table HWD_HelloWorld
 (
    id                   bigint not null,
    firstName            text,
